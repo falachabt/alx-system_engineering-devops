926270571e0b0e8732d60534006c092d3e21ad23
falachabt <bennytenezeu@gmail.com>
2024-07-26 04:30:27 +0100

    top_t

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-"""Module for task 1"""
-
+"""
+Module to query the Reddit API and print the titles of the first 10 hot posts listed for a given subreddit.
+"""
+import requests
 
 def top_ten(subreddit):
-    """Queries the Reddit API and returns the top 10 hot posts
-    of the subreddit"""
-    import requests
+    """
+    Function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
+    If not a valid subreddit, print None.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"User-Agent": "My User Agent 1.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
-        print('None')
+    if response.status_code == 200:
+        for post in response.json()['data']['children']:
+            print(post['data']['title'])
     else:
-        [print(child.get("data").get("title"))
-         for child in sub_info.json().get("data").get("children")]
+        print(None)

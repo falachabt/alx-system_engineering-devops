05d9bd982ed570d7a8d63bb2122b22e492c41027
falachabt <bennytenezeu@gmail.com>
2024-07-26 04:31:51 +0100

    top_t

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,12 +9,18 @@ def top_ten(subreddit):
     Function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
     If not a valid subreddit, print None.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    url = (
+        f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    )
     headers = {"User-Agent": "My User Agent 1.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        for post in response.json()['data']['children']:
-            print(post['data']['title'])
+        try:
+            data = response.json()
+            for post in data['data']['children']:
+                print(post['data']['title'])
+        except ValueError:
+            print(None)
     else:
         print(None)
